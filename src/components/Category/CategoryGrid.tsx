8e8d2e080ed9ef8bad6508647df380db9a25c043
biswo907 <biswajitdash907@gmail.com>
2025-10-17 00:07:19 +0530

    details page partially done

--- src/components/Category/CategoryGrid.tsx
+++ src/components/Category/CategoryGrid.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../../components/ui/style/CategoryGrid.module.scss";
+import { useRouter } from "next/navigation";
 
 interface CategoryGridProps {
   title: string;
@@ -7,12 +8,20 @@ interface CategoryGridProps {
 }
 
 const CategoryGrid: React.FC<CategoryGridProps> = ({ title, items }) => {
+  const router = useRouter();
+
   return (
     <section className={styles.categoryGrid}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.grid}>
         {items.map((item, index) => (
-          <button key={index} className={styles.gridItem}>
+          <button
+            onClick={() => {
+              router.push(`/categories/${item}`);
+            }}
+            key={index}
+            className={styles.gridItem}
+          >
             {item}
           </button>
         ))}
