323e0e0d38ac9df87d22c48fd3e9bb432fa36aa9
mirmeherbanali <mirmeherbanali9861@gmail.com>
2025-12-16 23:06:22 +0530

    nsee

--- src/components/ui/common/DesktopAuthLinks.tsx
+++ src/components/ui/common/DesktopAuthLinks.tsx
@@ -4,18 +4,22 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { getToken } from "@/utils/authStorage";
 import styles from "../../ui/style/Navbar.module.scss";
+import { usePathname } from "next/navigation";
 
 export default function DesktopAuthLinks() {
- 
+   const pathname = usePathname();
+
+  const isActive = (path: string) =>
+    pathname === path ? styles.active : "";
    const token = getToken()
   return (
     <>
       {token ? (
-        <Link href="/dashboard" className={styles.navItem}>
+        <Link href="/dashboard" className={`${styles.navItem} ${isActive("/dashboard")}`}>
           Dashboard
         </Link>
       ) : (
-        <Link href="/auth/login" className={styles.navItem}>
+        <Link href="/auth/login" className={`${styles.navItem} ${isActive("/auth/login")}`}>
           Login
         </Link>
       )}
--- src/components/ui/common/Navbar.tsx
+++ src/components/ui/common/Navbar.tsx
@@ -3,8 +3,14 @@ import Image from "next/image";
 import NavbarClient from "./NavbarClient";
 import DesktopAuthLinks from "./DesktopAuthLinks";
 import styles from "../../ui/style/Navbar.module.scss";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const isActive = (path: string) =>
+    pathname === path ? styles.active : "";
+  
   return (
     <header className={styles.header}>
       {/* LOGO */}
@@ -14,18 +20,19 @@ export default function Navbar() {
       </Link>
 
       {/* DESKTOP MENU */}
+      <div className={styles.navContainer}>
       <nav className={styles.desktopMenu}>
-        <Link href="/" className={styles.navItem}>Home</Link>
-        <Link href="/categories" className={styles.navItem}>Category</Link>
-        <Link href="/about" className={styles.navItem}>About</Link>
+        <Link href="/" className={`${styles.navItem} ${isActive("/")}`}>Home</Link>
+        <Link href="/categories" className={`${styles.navItem} ${isActive("/categories")}`}>Category</Link>
+        <Link href="/about" className={`${styles.navItem} ${isActive("/about")}`}>About</Link>
 
         {/* ✅ AUTH-CONTROLLED */}
         <DesktopAuthLinks />
       </nav>
-
       {/* MOBILE MENU */}
       <NavbarClient />
-      
+</div>
+
     </header>
-  );
+  ); 
 }
