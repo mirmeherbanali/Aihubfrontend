7534776b0e4500efcfccc678b2b3b5ea2190162a
mirmeherbanali <mirmeherbanali9861@gmail.com>
2025-12-21 22:01:30 +0530

    new

--- src/components/Home/HomeClient.tsx
+++ src/components/Home/HomeClient.tsx
@@ -1,17 +1,21 @@
 "use client";
-
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import PageHero from "@/components/Hero/PageHero";
 import Categories from "./categories";
 import FeaturedTools from "./FeaturedTools";
 import { useGetAllCategoriesQuery } from "@/features/dashboard/category/categoryApi";
 import { useGetAllToolsQuery } from "@/features/tools/toolsApi";
+import styles from "../ui/style/Home.module.scss";
 
 export default function HomeClient() {
   const router = useRouter();
 
   const { data: categoriesData } = useGetAllCategoriesQuery();
   const { data: toolsData } = useGetAllToolsQuery();
+  const [loadingToolId, setLoadingToolId] = useState<string | null>(null);
+
+  const [query, setQuery] = useState("")
 
   if (!categoriesData || !toolsData) return null;
 
@@ -35,17 +39,76 @@ export default function HomeClient() {
     );
   };
 
+  const filteredTools = query
+    ? tools.filter((tool: any) =>
+        tool.toolName.toLowerCase().includes(query.toLowerCase())
+      )
+    : [];
+
+  const handleSuggestionClick = (tool: any) => {
+    const categoryId = tool.category?.[0]?._id;
+    const category = categories.find((c: any) => c._id === categoryId);
+
+    if (!category) return;
+    // 🔥 show loading immediately
+    setLoadingToolId(tool._id);
+    router.push(
+      `/categories/${encodeURIComponent(
+        category.categoryName
+      )}/tooldetails/${encodeURIComponent(tool.toolName)}`
+    );
+    setQuery("");
+  };
+
+
   return (
     <>
       <PageHero
         content="Discover the Best AI Tools <br /> for Every Need"
         subcontent="Explore 1000+ AI tools categorized by use case, industry, pricing, and popularity."
         queryPlaceholder="Search for Tools & Categories"
-        onSearch={(query) => console.log("Searching:", query)}
+        liveSearch
+        onSearch={(val) => setQuery(val)}
         btnText="Add Your Tool"
         onBtnClick={() => router.push("/tool")}
       />
+{/* 🔽 SEARCH SUGGESTIONS */}
+      {query && filteredTools.length > 0 && (
+        <div className={styles.suggestionBox}>
+          {filteredTools.slice(0, 6).map((tool: any) => {
+            const categoryId = tool.category?.[0]?._id;
+            const category = categories.find(
+              (c: any) => c._id === categoryId
+            );
 
+            return (
+              <div
+                key={tool._id}
+                className={`${styles.suggestionItem} ${
+  loadingToolId === tool._id ? styles.loading : ""
+}`}
+                onClick={() => handleSuggestionClick(tool)}
+              >
+                <div className={styles.left}>
+                     {loadingToolId === tool._id ? (
+      <div className={styles.spinner}></div>
+    ) :(
+                  <img
+                    src={tool.logo || "/placeholder.png"}
+                    alt={tool.toolName}
+                  />
+    )}
+                </div>
+
+                <div className={styles.right}>
+                  <h4>{tool.toolName}</h4>
+                  <span> {loadingToolId === tool._id ? "Opening tool..." : category?.categoryName}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
       <Categories
         {...({ categoryData: categories, onCategoryClick: handleCategoryClick, onViewAllClick: handleViewAllClick } as any)}
       />
