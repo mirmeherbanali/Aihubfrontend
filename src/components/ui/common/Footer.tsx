8ba1d184b4f4cbfe5f0ecbfbd43b97dfe8e567a0
Dinesh32187 <97215747+Dinesh32187@users.noreply.github.com>
2026-03-07 00:21:17 +0530

    modifed done

--- src/components/ui/common/Footer.tsx
+++ src/components/ui/common/Footer.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import styles from "../style/footer.module.scss";
-import logoIMG from "../../../../public/assets/logo-allisted2.svg";
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
@@ -10,14 +9,7 @@ const Footer = () => {
         
         {/* Left Section */}
         <div className={styles.left}>
-           <Link href="/" className={styles.logo}>
-        <Image
-          src={logoIMG}
-          alt="Allisted Logo"
-          className={styles.logoImage}
-          priority
-        />
-      </Link>
+          <h2 className={styles.logo}>Allisted</h2>
           <p>
             Allisted is your go-to directory for the best AI tools. Explore,
             compare, and discover innovative solutions across categories —
